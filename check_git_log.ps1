d49a7534ce9ef71383a21667e5c9734b068ad16d
OmFukat-Dev <omfukat28@gmail.com>
2026-02-17 15:41:50 +0530

    Initial push with .gitignore for security

--- check_git_log.ps1
+++
@@ -1,3 +0,0 @@
-
-git status | Out-File -Encoding utf8 git_status.log
-git remote -v | Out-File -Encoding utf8 -Append git_status.log
--- setup_git_final.ps1
+++
@@ -1,8 +0,0 @@
-
-Start-Transcript -Path setup.log
-git add .
-git commit -m "Initial clean commit"
-git branch -M main
-git remote add origin https://github.com/OmFukat-Dev/Enterprise_Ai_Knowledge_Hub.git
-git push -u origin main
-Stop-Transcript
